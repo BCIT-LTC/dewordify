f6f044b44189fc506c3213e3eaec8a224b5a5746
Mike Taylor <mtaylor141@bcit.ca>
2016-03-24 13:49:44 -0700

    cleaned up folder structure and refactored paginate module

--- index.js
+++ index.js
@@ -1,20 +1,21 @@
 
-var toHTML = require("./lib/toHTML");
+var mammothize = require("./lib/mammothize");
 var normalize = require("./lib/normalize");
 var markout = require("./lib/markout");
 var paginate = require("./lib/paginate");
-var templateWrapper = require("./lib/templateWrapper");
-var writeFiles = require("./lib/writeFiles");
+var templatize = require("./lib/templatize");
+var fileWriter = require("./lib/fileWriter");
+var autoChoice = require("./lib/autoChoice");
 
-var docx = process.argv[2] || require("./lib/autoChoice")(process.cwd());
+var docx = process.argv[2] || autoChoice(process.cwd());
 
-toHTML(docx).then(function(_html) {
+mammothize(docx).then(function(_html) {
 	var html = _html;
+	var htmlArray;
 	
 	html = normalize(html);
 	html = markout(html);
-	var htmlPages = paginate(html);
-	htmlPages = templateWrapper(htmlPages);
-	
-	writeFiles(htmlPages);
-});;
+	htmlArray = paginate(html);
+	htmlArray = templatize(htmlArray);
+	fileWriter(htmlArray);
+});
