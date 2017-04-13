8a45a06efae8e95c665ad7631ca91585cd477571
Mike Taylor <A00751527@ad.bcit.ca>
2017-04-12 17:36:48 -0700

    Adds munching functionality

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var mammothize = require("./lib/mammothize");
 var normalize = require("./lib/normalize");
 var markout = require("./lib/markout");
@@ -7,27 +6,57 @@ var templatize = require("./lib/templatize");
 var fileWriter = require("./lib/fileWriter");
 var docxChooser = require("./lib/docxChooser");
 var statsTracker = require("./lib/statsTracker");
+var munch = require("./lib/munch");
 
-var docx = process.argv[2] || docxChooser(process.cwd());
-var verbose = false;
+module.exports = function(command) {
+	switch(command) {
+		case "munch":
+			munch();
+			break;
+		case "estimate":
+			estimate();
+			break;
+		default:
+			dewordify(true);
+	}
+	
+}
 
-mammothize(docx,verbose).then(function(_html) {
-	var html = _html;
-	var htmlArray;
-	var previewPage;
-	
-	
-	html = normalize(html);
-	htmlArray = paginate(html);
-	htmlArray = markout(htmlArray);
-	
-	// Add preview page
-	previewPage = htmlArray.join("");
-	htmlArray.push(previewPage); 
-	
-	htmlArray = templatize(htmlArray,".container");
-	fileWriter(htmlArray);
-	
-	// Evaluate preview page for statistics
-	statsTracker(previewPage); 
-});
+function dewordify(write) {
+	var docx = docxChooser(process.cwd());
+	var verbose = false;
+
+	mammothize(docx, verbose).then(function (_html) {
+		var html = _html;
+		var htmlArray;
+		var previewPage;
+
+
+		html = normalize(html);
+		htmlArray = paginate(html);
+		htmlArray = markout(htmlArray);
+
+		// Add preview page
+		previewPage = htmlArray.join("");
+		htmlArray.push(previewPage);
+
+		htmlArray = templatize(htmlArray, ".container");
+		
+
+		// Evaluate preview page for statistics
+		statsTracker(previewPage);
+
+		if(write) {
+			// write files
+			fileWriter(htmlArray);
+			
+			// munch file names
+			//munch(); // TODO: Ensure this only runs after the file writer is complete.
+		}
+		
+	});
+}
+
+function estimate() {
+	dewordify(false);
+}
